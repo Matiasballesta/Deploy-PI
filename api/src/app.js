125bad6afe0281eb7468e0154efd3d5be3405a79
Matiasballesta <75946070+Matiasballesta@users.noreply.github.com>
2022-07-04 12:15:29 -0300

    second done

--- api/src/app.js
+++ api/src/app.js
@@ -6,8 +6,6 @@ const routes = require('./routes/index.js');
 
 require('./db.js');
 
-
-
 const server = express();
 
 server.name = 'API';
@@ -17,19 +15,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:3000",
-   "https://pi-pokemon-9vrdkv08w-matiasballesta.vercel.app",
-   "https://pi-pokemon-six.vercel.app"
-    
-  ];
-  const origin = req.headers.origin || "";
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
